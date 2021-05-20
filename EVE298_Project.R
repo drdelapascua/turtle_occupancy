6129435dfa8c58e0434c55b07a3f0fc3b754da0a
mkriley <59615007+mkriley@users.noreply.github.com>
2021-05-20 16:38:42 -0700

    Add files via upload

--- EVE298_Project.R
+++ EVE298_Project.R
@@ -1,4 +1,4 @@
-# EVE 298: Group Project
+#### EVE 298: Group Project ####
 # Authors: Melissa, Sidney, Christofer, Danielle
 # Date: Spring Quarter 2021
 
@@ -6,23 +6,65 @@
 
 setwd("C:/Users/mkril/Desktop/EVE298")
 
-wpt <- read.csv("data/WPT_Occupancy_R.csv")
+wpt1 <- read.csv("data/WPT_Occupancy_R.csv")
+
+head(wpt1)
+str(wpt1)
+wpt1$fhabitat <- as.factor(wpt1$habitat)
+str(wpt1)
+
+# > lets do some data exploration! ----
+
+plot(number ~ salinity, data = wpt1)
+
+# response variable - number of wpt
+
+hist(wpt1$number)
+dotchart(wpt1$number, color = wpt1$fhabitat)
+
+# predictor variables - salinity, habitat type, air temp, water temperature, wind speed, flow status
+
+hist(wpt1$salinity)
+dotchart(wpt1$salinity)
+
+hist(wpt1$airtemp)
+dotchart(wpt1$airtemp) 
+
+hist(wpt1$watertemp)
+dotchart(wpt1$watertemp)
+
+hist(wpt1$avgwind)
+dotchart(wpt1$avgwind)
+
+hist(wpt1$flowstatus)
+dotchart(wpt1$flowstatus)
+
+#this is an observational data set, we should check to see if anything is co-linear
+#pairs(wpt)
+#pairs(loyn[,2:5]) #subsetting dataframes [rows, columns]
+#pairs(loyn[,c(2,4,6)])
+#pairs(loyn[,c("AREA", "DIST", "ABUND", "GRAZE")])
+
+
+#looking at the effect of habitat, but also looking at the spread at different habitat levels, may have to check homogeneity of variance closely
+boxplot(wpt1$number ~ wpt1$fhabitat)
+boxplot(wpt1$salinity ~ wpt1$fhabitat) 
+
+#### Data that is Summed ####
+
+wpt <- read.csv("data/WPT_edited.csv")
 
 head(wpt)
 str(wpt)
 wpt$fhabitat <- as.factor(wpt$habitat)
 str(wpt)
 
-'lets do some data exploration!'
-
-plot(number ~ salinity, data = wpt)
-
 # response variable - number of wpt
 
 hist(wpt$number)
 dotchart(wpt$number, color = wpt$fhabitat)
 
-# predictor variables - salinity, habitat type, air temp, water temperature, wind speed, flow status
+# predictor variables - salinity, habitat type, air temp, water temperature, wind speed, flow status, mgmt, basking
 
 hist(wpt$salinity)
 dotchart(wpt$salinity)
@@ -39,18 +81,37 @@ dotchart(wpt$avgwind)
 hist(wpt$flowstatus)
 dotchart(wpt$flowstatus)
 
-#this is an observational data set, we should check to see if anything is co-linear
-#pairs(wpt)
-#pairs(loyn[,2:5]) #subsetting dataframes [rows, columns]
-#pairs(loyn[,c(2,4,6)])
-#pairs(loyn[,c("AREA", "DIST", "ABUND", "GRAZE")])
+hist(wpt$baskingarea)
+dotchart(wpt$baskingarea)
 
-
-#looking at the effect of habitat, but also looking at the spread at different habitat levels, may have to check homogeneity of variance closely
 boxplot(wpt$number ~ wpt$fhabitat)
 boxplot(wpt$salinity ~ wpt$fhabitat) 
 
+# > preliminary linear model, gaussian distribution ----
 
+mod1 <- lm(number ~ salinity, data = wpt)
+summary(mod1)
+plot(mod1) # we have a trumpet which is bad
+
+mod2 <- lm((sqrt(number)) ~ salinity, data = wpt)
+summary(mod2)
+plot(mod2) # it looks worse
+
+# > poisson distribution ----
+mod.poisson <- glm(number ~ salinity, family = poisson, data = wpt)
+
+summary(mod.poisson) # residual deviance is way higher than the DF, so our model is over dispersed
+plot(mod.poisson)#residuals have a pattern
+
+AIC(mod2, mod.poisson, test = "Chi")
+# > adding a random effect for site ----
+# > model validation ----
+
+AIC(mod2, mod.poisson)
+
+#### Presence/Absence Data ####
+
+# > logistic regression ----
 
 
 #### Questions ####
@@ -60,5 +121,5 @@ boxplot(wpt$salinity ~ wpt$fhabitat)
 # some survey points were visited twice at different times of the year
 # type of model - gaussian vs. poisson distribution
 # what should be a random effect
-# what predictors should be included, is their colinearity between predictors
+# what predictors should be included, is there colinearity between predictors
 # managed vs. tidal, do we care about habitat types within these major categories?
